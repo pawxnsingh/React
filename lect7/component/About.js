25f1151ed48c19d5cd7c8df94bba4572ee4d5db8
Pawan Singh Dogra <psdograofficial@gmail.com>
2023-11-06 06:21:23 +0530

    Till lecture 7

---
+++ lect7/component/About.js
@@ -0,0 +1,13 @@
+import User from "./User";
+import UserClass from "./UserClass";
+
+const About = () => {
+    return (
+        <div>
+            <h1>About</h1>
+            <h2>This is Pawan singh dogra</h2>
+
+        </div>
+    );
+};
+export default About;
